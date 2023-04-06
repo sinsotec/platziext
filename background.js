1a2e15afef7c8b1b9f2e3239bb82e326e709b90b
Orlando Sanchez <pdigitalgroup@outlook.com>
2023-04-06 11:34:31 -0400

    Refactor and change Badge to Icon

--- background.js
+++ background.js
@@ -2,98 +2,75 @@ const platziClases = 'https://platzi.com/clases/'
 const platziBlog = 'https://platzi.com/blog/'
 const platziComments = 'https://platzi.com/comentario/'
 
+let state = {};
+
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.action.setBadgeText({
-        text: "OFF",
-    });
+    state = {};
 });
+
+chrome.tabs.onUpdated.addListener((tabID, change, tab) => {
+    if (state[tab.id]) {
+        state[tab.id] = {
+            url: tab.url,
+            state: false
+        };
+    }
+})
+
+const activateExt = async (tab, cssFile) => {
+
+    if (!state[tab.id]) {
+        state[tab.id] = {
+            url: tab.url,
+            state: true
+        };
+    } else if (state[tab.id].url != tab.url) {
+        state[tab.id] = {
+            url: tab.url,
+            state: true
+        };
+    } else {
+        state[tab.id].state = !state[tab.id].state;
+    }
+
+    if (state[tab.id].state) {
+        // Insert the CSS file change icon when the user turns the extension on
+        await chrome.scripting.insertCSS({
+            files: [cssFile],
+            target: { tabId: tab.id }
+        });
+        await chrome.action.setIcon({
+            tabId: tab.id,
+            path: {
+                "32": "images/platzitoolza_blanco-32.png"
+            }
+        });
+    } else {
+        // Remove the CSS file and change icon when the user turns the extension off
+        await chrome.scripting.removeCSS({
+            files: [cssFile],
+            target: { tabId: tab.id }
+        });
+        await chrome.action.setIcon({
+            tabId: tab.id,
+            path: {
+                "32": "images/platzitoolza2_negro-32.png"
+            }
+        });
+    }
+}
 
 
 chrome.action.onClicked.addListener(async (tab) => {
     if (tab.url.startsWith(platziClases)) {
-        
-        // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
-        const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-        // Next state will always be the opposite
-        const nextState = prevState === 'ON' ? 'OFF' : 'ON'
-
-        // Set the action badge to the next state
-        await chrome.action.setBadgeText({
-            tabId: tab.id,
-            text: nextState,
-        });
-
-        if (nextState === 'ON') {
-            // Insert the CSS file when the user turns the extension on
-            await chrome.scripting.insertCSS({
-                files: ['hideComments.css'],
-                target: { tabId: tab.id }
-            });
-        } else if (nextState === 'OFF') {
-            // Remove the CSS file when the user turns the extension off
-            await chrome.scripting.removeCSS({
-                files: ['hideComments.css'],
-                target: { tabId: tab.id }
-            });
-        }
+        await activateExt(tab, 'hideComments.css')
     }
-
-
     if (tab.url.startsWith(platziBlog)) {
-        // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
-        const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-        // Next state will always be the opposite
-        const nextState = prevState === 'ON' ? 'OFF' : 'ON'
-
-        // Set the action badge to the next state
-        await chrome.action.setBadgeText({
-            tabId: tab.id,
-            text: nextState,
-        });
-
-        if (nextState === 'ON') {
-            // Insert the CSS file when the user turns the extension on
-            await chrome.scripting.insertCSS({
-                files: ['darkmodeBlog.css'],
-                target: { tabId: tab.id }
-            });
-        } else if (nextState === 'OFF') {
-            // Remove the CSS file when the user turns the extension off
-            await chrome.scripting.removeCSS({
-                files: ['darkmodeBlog.css'],
-                target: { tabId: tab.id }
-            });
-        }
+        await activateExt(tab, 'darkmodeBlog.css')
     }
 
     if (tab.url.startsWith(platziComments)) {
-        // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
-        const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-        // Next state will always be the opposite
-        const nextState = prevState === 'ON' ? 'OFF' : 'ON'
-
-        // Set the action badge to the next state
-        await chrome.action.setBadgeText({
-            tabId: tab.id,
-            text: nextState,
-        });
-
-        if (nextState === 'ON') {
-            // Insert the CSS file when the user turns the extension on
-            await chrome.scripting.insertCSS({
-                files: ['darkmodeComments.css'],
-                target: { tabId: tab.id }
-            });
-        } else if (nextState === 'OFF') {
-            // Remove the CSS file when the user turns the extension off
-            await chrome.scripting.removeCSS({
-                files: ['darkmodeComments.css'],
-                target: { tabId: tab.id }
-            });
-        }
+        await activateExt(tab, 'darkmodeComments.css')
     }
-
-
-
 });
 
